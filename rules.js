dca695c90e60dcb750579443e7441fee6af206d8
Eric Cai <Eric021202@gmail.com>
2025-04-18 23:47:36 -0700

    updated mystory and rules

--- rules.js
+++ rules.js
@@ -1,37 +1,73 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title);
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation);
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        this.key = key; // Store current key for reference
+        const locationData = this.engine.storyData.Locations[key];
+        this.engine.show(`<p>${locationData.Body}</p>`);
+
+        // Show object if present
+        if (locationData.Object) {
+            this.engine.show(`<p><strong>You see:</strong> ${locationData.Object.Description}</p>`);
+            this.engine.addChoice(locationData.Object.ActionText, { useObject: true, objectEffect: locationData.Object.Effect });
+        }
+
+        // Add story choices
+        if (locationData.Choices) {
+            for (let choice of locationData.Choices) {
+                this.engine.addChoice(choice.Text, choice);
             }
         } else {
-            this.engine.addChoice("The end.")
+            this.engine.addChoice("The end.");
         }
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
-        } else {
-            this.engine.gotoScene(End);
+    // Handle object interaction first
+    if (choice?.useObject) {
+        this.engine.show(`<p><em>${choice.objectEffect}</em></p>`);
+        this.create(); // Refresh current scene
+        return;
+    }
+
+    // Check for required flags if they exist
+    if (choice?.RequiredFlags) {
+        const missingFlags = choice.RequiredFlags.filter(flag => !this.engine.hasFlag(flag));
+        if (missingFlags.length > 0) {
+            this.engine.show(`<p><em>You can't go there yet. You might be missing something...</em></p>`);
+            this.engine.addChoice("Return to Campus Quad", { Target: "CampusQuad" });
+            return;
         }
     }
+
+    // Handle MazeTruck special case
+    if (this.key === "MazeTruck") {
+        const options = ["RamenTruck", "TacoTruck", "HotDogTruck", "DumplingTruck"];
+        const randomTarget = options[Math.floor(Math.random() * options.length)];
+        this.engine.show(`> You step into the unknown...`);
+        this.engine.gotoScene(Location, randomTarget);
+        return;
+    }
+
+    // Handle normal scene transitions
+    if (choice?.Target) {
+        this.engine.show(`> ${choice.Text}`);
+        this.engine.gotoScene(Location, choice.Target);
+        return;
+    }
+
+    // Default case (should only reach here for "TheEnd")
+    this.engine.gotoScene(End);
+}
 }
 
 class End extends Scene {
